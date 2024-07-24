9623bf423e73a8991c30e69c962b25de71e5d1a6
Olegs <olegs.krivko@gmail.com>
2024-07-24 15:01:54 +0300

    fix loc

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ const app = express();
 // Rate limiting configuration
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes window
-  max: 100, // Limit each IP to 100 requests per windowMs
+  max: 500, // Limit each IP to 100 requests per windowMs
   message: "Too many requests from this IP, please try again later.",
 });
 
